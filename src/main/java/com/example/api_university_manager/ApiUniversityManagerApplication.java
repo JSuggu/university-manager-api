ac36b765854079e9acdcbc97179a56215113c980
Franco Azame <azamefranco@gmail.com>
2024-07-07 13:36:15 -0300

    project started

---
+++ src/main/java/com/example/api_university_manager/ApiUniversityManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_university_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUniversityManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiUniversityManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/api_university_manager/ApiUniversityManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_university_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUniversityManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
